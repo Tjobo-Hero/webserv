3d9e5e7e6771c4d3299bcf4a5b7e0bd37ea0c540
Tim van Citters <timvcitters@gmail.com>
2021-05-25 14:57:51 +0200

    good day and wonderfull manual by beej

--- Preperation/medium_tutorial/tcp_server_code.c
+++ Preperation/medium_tutorial/tcp_server_code.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   working_on_them_http_stuff.c                       :+:    :+:            */
+/*   tcp_server_code.c                                  :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: timvancitters <timvancitters@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/20 12:05:06 by timvancitte   #+#    #+#                 */
-/*   Updated: 2021/05/20 12:39:44 by timvancitte   ########   odam.nl         */
+/*   Updated: 2021/05/25 13:08:20 by timvancitte   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(void)
-{
+// int main(void)
+// {
 	
-	int server_fd;
-// Step 1. Create a socket
+// +++++ STEP 1 Create a socket +++++
 // A socket, server_fd, is created with the socket system call:
 // int server_fd = socket(domain, type, protocol);
 
@@ -42,13 +45,13 @@ int main(void)
 // there are no variations of the protocol, so the last argument, 
 // protocol, is zero. Our code for creating a TCP socket looks like this:
 
-if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-{
-	perror("cannot create socket");
-	return 0;
-}
+// if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+// {
+// 	perror("cannot create socket");
+// 	return 0;
+// }
 
-// Step 2. Identify (name) a socket
+// +++++ STEP 2. Identify (name) a socket +++++
 // When we talk about naming a socket, we are talking about assigning 
 // a transport address to the socket (a port number in IP networking). 
 // In sockets, this operation is called binding an address and the bind 
@@ -115,31 +118,33 @@ if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 
 // The code to bind a socket looks like this:
 
-struct sockaddr_in address;
-const int PORT = 8080; // Where the cleints can reach at
+// const int PORT = 8080; // Where the cleints can reach at
 
 /*htonl converts a long integer (e.g. address)to a network representation */
 /*htons converts a short integer (e.g. port) to a network representation */
 
-memset((char*)&address, 0, sizeof(address)); // Set every variable in the struct to zero. 
-address.sin_family = AF_INET;
-address.sin_addr.s_addr = htonl(INADDR_ANY);
-address.sin_port = htons(PORT);
+// memset((char*)&address, 0, sizeof(address)); // Set every variable in the struct to zero. 
+// address.sin_family = AF_INET;
+// address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-{
-	perror("bind failed");
-	return 0;
-}
+// address.sin_port = htons(PORT);
 
-// Step 3. On the server wait for an incoming connection
+// printf("INADDR_ANY: %i\n", INADDR_ANY);
+
+// if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+// {
+// 	perror("bind failed");
+// 	return 0;
+// }
+
+// +++++ STEP 3. On the server wait for an incoming connection +++++
 //
 // Before a client can connect to a server, the server 
 // should have a socket that is prepared to accept the connections. 
 // The listen system call tells a socket that it should be capable 
 // of accepting incoming connections:
 //
-// int listen(int socket, int backlog)
+// int listen(int socket, int backlog);
 // 
 // backlog: defines the maximum number of pending connections that 
 // can be queued up before connections are refused.
@@ -152,5 +157,119 @@ if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
 // is used only for accepting connections, not for exchanging 
 // data. By default, socket operations are synchronous, or blocking, 
 // and accept will block until a connection is present on the queue.
+
+// int accept(int socket, struct sockaddr *restrict address, socklen_t *restric address_len);
 // 
+// socket: is the socket that was set for accpeting connetions with listen.
+//
+// address: is the address structure that gets filed in with the addresss of the client
+// that is doing the connect. This allows us to examine the address and port number of the 
+// connecting socket if we want to. 
+// 
+// address_len: the length of the address structure.
+// 
+// The code to listen and accept look like:
+
+// int new_socket;
+
+// if (listen(server_fd, 3) < 0)
+// {
+// 	perror("In listen");
+// 	exit(EXIT_FAILURE);
+// }
+
+// if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+// {
+// 	perror("In Accept");
+// 	exit(EXIT_FAILURE);
+// }
+
+// +++++ STEP 4 Send and receive messages +++++
+// We finally have connected sockets between a client (when you visit IP address of your server
+// from a web browser) and a server!
+//
+// Communication is the easy part. The same read and write system calls
+// that work on files also work on sockets
+//
+
+// char buffer[1024] = {0};
+
+// int valread = read(new_socket, buffer, 1024);
+// printf("%s\n", buffer);
+
+// if (valread < 0)
+// {
+// 	printf("No bytes are there to read");
+// }
+// char *hello = "Hello from the server"; // IMPORTANT! WE WILL GET TO IT
+// write(new_socket, hello, strlen(hello));
+// Note: the real working of HTTP server happens based on the content
+// present in char *hello variable. We will get back to it later
+
+// +++++ STEP 5 Close the socket +++++
+// When we're done communicating, the easiest thing to do is to close a socket
+// with the close system call the same close that is used for files
+
+// close(new_socket);
+// }
+
+#define PORT 8080
+int main(int argc, char **argv)
+{
+	int server_fd;
+	int new_socket;
+	long valread;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
+	int yes = 1;
+
+	char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+	
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("In socket");
+		exit(EXIT_FAILURE);
+	}
+	
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+	address.sin_port = htons(PORT);
+	
+	memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+	
+	if (setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) 
+	{
+    	perror("setsockopt");
+    	exit(1);
+	}
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+	{
+		perror("In bind");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("In accept");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		printf("\n++++++++ Waiting for new connection +++++++\n\n");
+		if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
+		{
+			perror("In accept");
+			exit(EXIT_FAILURE);
+		}
+
+		char buffer[30000] = {0};
+		valread = read(new_socket, buffer, 30000);
+		printf("%s\n", buffer);
+		write(new_socket, hello, strlen(hello));
+		printf("------Hello message sent-----\n");
+		close(new_socket);
+	}
+	return 0;
+	
 }
+
