276d9ecdd3a39fd0e510508aa7cc9f3a2bce6c41
Tim van Citters <timvcitters@gmail.com>
2021-06-14 12:29:43 +0200

    presumably done with the parser, need to check if it compiles, nice job to do with a full belly

--- This_is_it/playaround/main.cpp
+++ This_is_it/playaround/main.cpp
@@ -6,70 +6,151 @@
 /*   By: timvancitters <timvancitters@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/10 10:37:24 by timvancitte   #+#    #+#                 */
-/*   Updated: 2021/06/11 12:29:23 by timvancitte   ########   odam.nl         */
+/*   Updated: 2021/06/14 11:26:05 by timvancitte   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <sstream>
+#include <vector>
 
 
-const std::string WHITESPACE = " \n\r\t\f\v";
-
-std::string removeLeadingSpaces(const std::string &line) {
-	size_t start;
-	
-	start = line.find_first_not_of(WHITESPACE, 0);
-	if (start == std::string::npos)
-		return "";
-	else
-		return line.substr(start);
-}
-
-std::string removeTrailingSpaces(const std::string &line) {
-	size_t end;
-	
-	end = line.find_last_not_of(WHITESPACE);
-	if (end == std::string::npos)
-		return "";
-	else
-		return line.substr(0, end + 1);
-}
-
-std::string removeLeadingAndTrailingSpaces(const std::string &line) {
-	return removeTrailingSpaces(removeLeadingSpaces(line));
-}
 
 int main()
 {
-	std::string line = "   asdf :     ";
-	std::string user;
-	std::string password;
-	const char*	delimiter = ":";
-	const char* endOfFile = "\n\r#;";
-
-	size_t userBegin = line.find_first_not_of(delimiter);
-	size_t userEnd = line.find_first_of(delimiter, userBegin);
+	std::vector<std::string> result;
+	std::string dit = "robijn maarten wester tim";
+	std::stringstream ss(dit);
+	std::string hoi;
 	
-	user = line.substr(userBegin, userEnd - userBegin);
-	user = removeLeadingAndTrailingSpaces(user);
-	if (user.empty())
-	{
-		std::cout << "No user added" << std::endl;
-		return 1;
-	}
-	size_t passwordBegin = line.find_first_not_of(delimiter, userEnd);
-	size_t passwordEnd = line.find_first_of(endOfFile, passwordBegin);
-
-	password = line.substr(passwordBegin, passwordEnd - passwordBegin);
-	password = removeLeadingAndTrailingSpaces(password);
-	if (password.empty())
-	{
-		std::cout << "No password for: " + user + "added" << std::endl;
-		return 1;
-	}
-
-	std::cout << "User: [" << user << "]" << " password: [" << password << "]" << std::endl;
+	while (ss >> hoi)
+		result.push_back(hoi);
+	std::vector<std::string>::iterator it = result.begin();
+	for (; it != result.end(); ++it)
+		std::cout << *it << std::endl;
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const std::string WHITESPACE = " \n\r\t\f\v";
+
+// std::string removeLeadingSpaces(const std::string &line) {
+// 	size_t start;
+	
+// 	start = line.find_first_not_of(WHITESPACE, 0);
+// 	if (start == std::string::npos)
+// 		return "";
+// 	else
+// 		return line.substr(start);
+// }
+
+// std::string removeTrailingSpaces(const std::string &line) {
+// 	size_t end;
+	
+// 	end = line.find_last_not_of(WHITESPACE);
+// 	if (end == std::string::npos)
+// 		return "";
+// 	else
+// 		return line.substr(0, end + 1);
+// }
+
+// std::string removeLeadingAndTrailingSpaces(const std::string &line) {
+// 	return removeTrailingSpaces(removeLeadingSpaces(line));
+// }
+
+// int main()
+// {
+// 	std::string line = "   asdf :     ";
+// 	std::string user;
+// 	std::string password;
+// 	const char*	delimiter = ":";
+// 	const char* endOfFile = "\n\r#;";
+
+// 	size_t userBegin = line.find_first_not_of(delimiter);
+// 	size_t userEnd = line.find_first_of(delimiter, userBegin);
+	
+// 	user = line.substr(userBegin, userEnd - userBegin);
+// 	user = removeLeadingAndTrailingSpaces(user);
+// 	if (user.empty())
+// 	{
+// 		std::cout << "No user added" << std::endl;
+// 		return 1;
+// 	}
+// 	size_t passwordBegin = line.find_first_not_of(delimiter, userEnd);
+// 	size_t passwordEnd = line.find_first_of(endOfFile, passwordBegin);
+
+// 	password = line.substr(passwordBegin, passwordEnd - passwordBegin);
+// 	password = removeLeadingAndTrailingSpaces(password);
+// 	if (password.empty())
+// 	{
+// 		std::cout << "No password for: " + user + "added" << std::endl;
+// 		return 1;
+// 	}
+
+// 	std::cout << "User: [" << user << "]" << " password: [" << password << "]" << std::endl;
+	
+// 	return 0;
+// }
